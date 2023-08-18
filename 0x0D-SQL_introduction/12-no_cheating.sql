0d9e615b565b0440ef78c4af7fa25ee43156199d
safwana2 <alhassan0540221766@icloud.com>
2023-08-17 22:46:23 -0700

    o

---
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -0,0 +1,9 @@
+test commit
+Tolulope05
+Tolulope05
+sql
+
+-- Updates the score of Bob to 10 in the table second_table in my MySQL server.
+UPDATE `second_table`
+SET `score` = 10
+WHERE `name` = "Bob";
